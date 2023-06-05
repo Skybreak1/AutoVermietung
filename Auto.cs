617b818875c36f70ab425ab71c30fd92f8ce3c6f
Skybreak1 <133328162+Skybreak1@users.noreply.github.com>
2023-06-05 16:19:18 +0200

    aktueller stand

---
+++ Auto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoVermietung
+{
+    internal class Auto
+    {
+        private int AutoNr;
+        private String Hersteller;
+        private String Modell;
+        private int PS;
+        private string Baujahr;
+        private float Mietpreis;
+        private bool Verfuegbar;
+
+        public Auto(int autoNr, string hersteller, string modell, int pS, string baujahr, float mietpreis, bool verfuegbar)
+        {
+            AutoNr = autoNr;
+            Hersteller = hersteller;
+            Modell = modell;
+            PS = pS;
+            Baujahr = baujahr;
+            Mietpreis = mietpreis;
+            Verfuegbar = verfuegbar;
+        }
+    }
+}
---
+++ Kunde.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoVermietung
+{
+    internal class Kunde
+    {
+        private int KundenNr;
+        private string Vorname;
+        private string Nachname;
+        private string Adresse;
+        private DateTime KundeSeit;
+
+        public Kunde(int kundenNr, string vorname, string nachname, string adresse, DateTime kundeSeit)
+        {
+            KundenNr = kundenNr;
+            Vorname = vorname;
+            Nachname = nachname;
+            Adresse = adresse;
+            KundeSeit = kundeSeit;
+        }
+        int GetKundenNr()
+        {
+            return KundenNr;
+        }
+        string GetName()
+        {
+            return Vorname + "" + Nachname;
+        }
+        string GetAdresse()
+        {
+            return Adresse;
+        }
+        DateTime GetKundeSeit()
+        {
+            return KundeSeit;
+        }
+        
+
+        
+    }
+}
